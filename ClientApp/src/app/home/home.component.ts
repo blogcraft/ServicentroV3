ccfbb7bb844ef4e3c08b70501a06558a425ddea8
Gerardo Buenrostro G <blogcraft@gmail.com>
2018-09-27 02:23:22 -0300

    Pantalla de inicio y animaciones

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   pagina: string;
